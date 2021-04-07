31377f1d0ddae0e9c55a6325be2787004a62f1a6
heathersmith <heathermachelle.s@gmail.com>
2021-04-07 13:39:36 -0600

    finished 3rd section

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
@@ -1,7 +1,6 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,7 +8,7 @@ import java.util.List;
 public class Job extends AbstractEntity{
 
     @ManyToOne
-    @NotNull(message = "Job required")
+
     private Employer employer;
 
     @ManyToMany
